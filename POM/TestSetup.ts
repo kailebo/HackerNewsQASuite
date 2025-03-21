f2e195a12c793d4d022fb38f6a33cb2b483e38ee
kailebo <kaileb.oneil@gmail.com>
2025-03-21 18:49:42 -0400

    POM, Test and test data coded. Test is timeout failing at submit check

---
+++ POM/TestSetup.ts
@@ -0,0 +1,19 @@
+import { type Locator, type Page } from "playwright/test";
+
+//POM to open and close browsers for test on Hacker News
+
+export class TestSetup {
+    readonly page: Page;
+    readonly hackerNewsURL: string;
+
+    constructor(page: Page) {
+        this.page = page
+        this.hackerNewsURL = 'https://news.ycombinator.com/'
+    }
+    async gotoHackerNews() {
+        await this.page.goto(this.hackerNewsURL, {waitUntil: "load", timeout: 120000});
+    }
+    async closePage() {
+        await this.page.close()
+    }
+}
